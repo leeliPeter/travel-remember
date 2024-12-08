7bac47e7835a3c00714a257b048566059ff3efc9
Lei Ieong Tam <leeli.petertam@gmail.com>
2024-12-07 21:43:52 -0500

    refresh after signout

--- actions/signOut.ts
+++ actions/signOut.ts
@@ -1,6 +1,7 @@
 "use server";
-import {signOut} from "@/auth";
+import { signOut } from "@/auth";
 
 export const signOutAction = async () => {
-    await signOut();
-    }
+  await signOut();
+  return Response.redirect(new URL("/", process.env.NEXT_PUBLIC_APP_URL!));
+};
