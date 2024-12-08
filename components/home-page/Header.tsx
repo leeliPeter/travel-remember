c16e4bac43d1bf8d9008b6779d821f71a3751542
Lei Ieong Tam <leeli.petertam@gmail.com>
2024-12-07 23:08:15 -0500

    home page image

--- components/home-page/Header.tsx
+++ components/home-page/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="py-40 md:py-60 md:pb-48">
+    <header className="py-40 md:py-60 2xl:py-80 4xl:py-96 md:pb-48">
       <div className=" container mx-auto p-6 ">
         <div className="border-2 border-white max-w-sm md:max-w-md mx-6 md:ml-10 p-4 md:p-8">
           <div className="text-white uppercase text-3xl sm:text-4xl md:text-5xl font-semibold  md:leading-[60px] ">
